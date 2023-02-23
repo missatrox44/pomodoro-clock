1d0093222c71c742888b248286791c4144399703
Developer <Developer@Johns-MacBook-Air-2.local>
2023-02-23 17:53:39 -0500

    Using pomofocus.io as a mock-up, I broke the UI down into components. I've also cleaned out the default css that comes with a Vite scaffold, and began working on styles. Probably should have done this last part in a different feature branch. Will consider that moving forward.

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,92 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+function TopMenu(){
+  //TODO
+  return(
+    <nav className='top-menu'>
+      <span>Pomodoro Timer</span>
+      <button>Report</button>
+      <button>Setting</button>
+      <button>Login</button>
+    </nav>
+  )
+}
+function ControlArea(){
+  return(
+    <div className='ControlArea'>
+      <ControlMenu />
+      <Clock time={'25:00'}/>
+      <button className='start-button'>START</button>
     </div>
+  )
+  //TODO
+}
+function ControlMenu(){
+  return (
+    <div className='ControlMenu'>
+      <button>Pomodoro</button>
+      <button>Short Break</button>
+      <button>Long Break</button>
+    </div>
+  )
+  //TODO
+}
+function Clock({time}){
+  return(
+    <span className='clock-readout'>{time}</span>
+  )
+  //TODO
+}
+function TaskArea() {
+  //TODO
+  return(
+    <>
+      <button className="add-task">Add Task</button>
+      <TasksList />
+    </>
   )
 }
 
-export default App
+function TasksList() {
+  //TODO
+  return (
+    <div className='task-list'>
+    <TaskListItem name="Stuff." />
+    <TaskListItem name="More stuff." />
+    <TaskListItem name="Even more stuff." />
+    </div>
+  )
+}
+function TaskListItem({name}){
+  return (
+    <div className='task-list-item'>
+      <span>{name}</span>
+      <span>0/1</span>
+    </div>
+  )
+}
+function PomodoroCreationMenu(){
+  //TODO
+  return(
+    <div className='pomodoro-creation-menu'>
+      <input>What are you working on?</input>
+      <span>Est Pomodoros</span>
+      <span>1</span>
+      <button>+1</button><button>-1</button>
+    </div>
+  )
+  
+}
+
+function App() {
+  //TODO
+  return (
+    <>
+      <TopMenu/>
+      <ControlArea />
+      <TaskArea />
+    </>
+    )
+}
+export default App;
