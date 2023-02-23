9a6bd13c6c5cec9b32a84b57bc331c02da2e715b
Developer <Developer@Johns-MacBook-Air-2.local>
2023-02-23 18:21:32 -0500

    Styled the top-menu and the task-list-items a bit. Also added a base8 image for the menu icon on the task-list-items. Definitely want to replace this with svg when I can, so we can have a proper hover effect.

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,11 @@ function TopMenu(){
   return(
     <nav className='top-menu'>
       <span>Pomodoro Timer</span>
-      <button>Report</button>
-      <button>Setting</button>
-      <button>Login</button>
+      <div>
+        <button>Report</button>
+        <button>Setting</button>
+        <button>Login</button>
+      </div>
     </nav>
   )
 }
@@ -64,6 +66,7 @@ function TaskListItem({name}){
     <div className='task-list-item'>
       <span>{name}</span>
       <span>0/1</span>
+      <img className="menu-icon" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAYAAAAeP4ixAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAwUlEQVR4nO3ZQQ6CMBSE4X8jnMF6Q6zeUoREDVcxuNc0KUuF7qZmvuTt2EwetJkAZlVpgRMwAK88VyACDZU4ABPw/jIPIFDBJqYfIZa5q2/mvCHEMkeEjQVB0jcjay4Ikp79iyBPhA0FQXqExYIgHcKafE+shbgBO8SFlTApxJ5KNPme6PMBkOaSXyf5TZjZdu4jKtxH1IzuI2Jm9xEx0X1EUHAfMbPqtP4/IsJ9RM3oPiJmdh8RE91HBAX3ETPkfADfe0+kCAYLvQAAAABJRU5ErkJggg=="/>
     </div>
   )
 }
