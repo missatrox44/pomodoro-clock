d827b53cfd1c6a00cb9282ad31f6d47a828ee50a
Sara Baqla <missatrox44@gmail.com>
2023-02-23 19:27:30 -0600

    added state components

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
 
 function Modal() {
+  const [roundsEstimated, setRoundsEstimated] = useState(1)
+  
   return (
     <div className='pomodoro-creation-menu'>
     {/* <input type='text'>What are you working on?</input> */}
---
+++ src/components/Rounds.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useState } from 'react'
+
+function Rounds() {
+ const [roundsCompleted, setRoundsCompleted] = useState(0)
+
+  return (
+    <div>Rounds</div>
+  )
+}
+
+export default Rounds
