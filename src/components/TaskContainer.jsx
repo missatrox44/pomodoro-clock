0fc1e08a89cd419f64c43eed16c6bc2a355407cf
Sara Baqla <missatrox44@gmail.com>
2023-02-26 10:14:36 -0600

    renders tasksList as Task component

--- src/components/TaskContainer.jsx
+++ src/components/TaskContainer.jsx
@@ -1,13 +1,10 @@
 import React from "react";
 import Task from "./Task";
 import { useState } from "react";
-// import Modal from "./Modal";
+import { v4 } from "uuid";
 
 function TaskContainer() {
   const [isHideModal, setIsHideModal] = useState(true);
-  // const [roundsEstimated, setRoundsEstimated] = useState(1);
-  // const [input, setInput] = useState("");
-  // const [taskData, setTaskData] = useState({name:'', estimated:1});
   const [name, setName] = useState("");
   const [estimated, setEstimated] = useState(1);
   const [tasksList, setTasksList] = useState([]);
@@ -16,17 +13,14 @@ function TaskContainer() {
     setIsHideModal(false);
   }
 
-  //function that tracks value of name input
   function handleNameChange(event) {
     setName(event.target.value);
   }
 
   function handleEstimatedChange(event) {
-    //totally copy and pasted the next line but i know that Math.max is used to make sure the number is not negative. parseInt takes the value of input and turns it into a number, might be overkill
     setEstimated(Math.max(parseInt(event.target.value), 0));
   }
 
-  //decrement function
   function minusOne() {
     if (estimated > 0) {
       setEstimated(estimated - 1);
@@ -35,23 +29,16 @@ function TaskContainer() {
     }
   }
 
-  //increment function
   function addOne() {
     setEstimated(estimated + 1);
   }
 
-  //Save button functionality where the magic happens
   function handleSubmit(event) {
     event.preventDefault();
-    //define task object
     const task = { name, estimated, actual: 0, completed: false };
-    //pushes task object to tasksList array
     setTasksList([...tasksList, task]);
-    console.log("task added", task);
-    console.log("tasksList", tasksList);
-    //clears out input
     setName("");
-    setEstimated(0);
+    setEstimated(1);
     setIsHideModal(true);
   }
 
@@ -60,9 +47,21 @@ function TaskContainer() {
     console.log("pressed cancel button");
   }
 
+  const tasks = tasksList.map((task) => {
+    return (
+      <Task
+        name={task.name}
+        estimated={task.estimated}
+        actual={task.actual}
+        completed={task.completed}
+        id={v4()}
+      />
+    );
+  });
+
   return (
     <div className="task-container">
-      <Task />
+      {tasks}
       <button onClick={showModal} className="add-task">
         Add Task
       </button>
@@ -90,7 +89,7 @@ function TaskContainer() {
                 <span className="material-icons icons">arrow_drop_down</span>
               </button>
               <button type="button" onClick={addOne}>
-               <span className="material-icons icons">arrow_drop_up</span>
+                <span className="material-icons icons">arrow_drop_up</span>
               </button>
               <button onClick={cancelBtnClicked}>Cancel</button>
               <button type="submit">Save</button>
@@ -104,35 +103,4 @@ function TaskContainer() {
 
 export default TaskContainer;
 
-
-
-// Old form:
-//             {/* <form> */}
-
 //             {/* https://dev.to/deboragaleano/how-to-handle-multiple-inputs-in-react-55el */}
-//             {/*
-//         <input
-//           type="text"
-//           className="input-field"
-//           name="task"
-//           placeholder="What are you working on?"
-//           onChange={handleChange}
-//           value={taskData.name}
-//         />
-//           <input
-//             type="text"
-//             className="input-field"
-//             name="num"
-//             placeholder="1"
-//             onChange={handleChange}
-//             value={taskData.estimated}
-//           />
-//           <button onClick={addOne}>+1</button>
-//           <button onClick={minusOne}>-1</button>
-
-//           <div>
-//             <button onClick={()=>{cancel()}}>Cancel</button>
-//             <button onClick={()=>{save()}}>Save</button>
-//           </div>
-
-//         </form> */}
