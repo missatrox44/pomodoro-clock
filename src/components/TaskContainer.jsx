16570ab738bff710be3cc10198404674eb39041b
Developer <Developer@Johns-MacBook-Air-2.local>
2023-02-25 19:00:19 -0500

    stuff.

--- src/components/TaskContainer.jsx
+++ src/components/TaskContainer.jsx
@@ -3,31 +3,38 @@ import Task from "./Task";
 import { useState } from "react";
 // import Modal from "./Modal";
 
+const tasks = []
+
+function createTask(name, est, actual=0, completed=false){
+  const task = { name: 'name', est: 0, actual: 0, completed: Boolean } 
+  tasks.push(task);
+  console.log(task);
+}
+
 function TaskContainer() {
   const [isHideModal, setIsHideModal] = useState(true);
-
-  const [roundsEstimated, setRoundsEstimated] = useState(1);
-
-  const [input, setInput] = useState("");
+  // const [roundsEstimated, setRoundsEstimated] = useState(1);
+  // const [input, setInput] = useState("");
+  const [taskData, setTaskData] = useState({name:'', estimated:1});
 
   function showModal() {
     setIsHideModal(false);
   }
 
   function handleChange(e) {
-    // setInput;
+    setInput;
   }
 
   function addOne() {
-    setRoundsEstimated(roundsEstimated + 1);
+    setTaskData(taskData.estimated + 1);
   }
 
   function minusOne() {
-    if (roundsEstimated > 0) {
-      setRoundsEstimated(roundsEstimated - 1);
+    if (taskData.estimated > 0) {
+      setTaskData(taskData.estimated - 1);
     }
     else {
-      setRoundsEstimated(0)
+      setTaskData.estimated(0)
     }
   }
 
@@ -49,24 +56,27 @@ function TaskContainer() {
 
       <div className={`${isHideModal ? "hidden" : ""}`}> 
       <div className="pomodoro-creation-menu">
+      
+      <p>Est Pomodoros</p>
+      <div>
+        <form>
       <input
         type="text"
         className="input-field"
         name="task"
         placeholder="What are you working on?"
         onChange={handleChange}
-        // value={task}
+        value={taskData.name}
       />
-      <p>Est Pomodoros</p>
-      <div>
         <input
           type="text"
           className="input-field"
           name="num"
           placeholder="1"
           onChange={handleChange}
-          value={roundsEstimated}
+          value={taskData.estimated}
         />
+        </form>
         <button onClick={addOne}>+1</button>
         <button onClick={minusOne}>-1</button>
       </div>
