7a4d38f53418dfb417eb9e4081ce68cc19286570
Sara Baqla <missatrox44@gmail.com>
2023-02-27 23:14:20 -0600

    resolved conflicts in task container

--- src/components/TaskContainer.jsx
+++ src/components/TaskContainer.jsx
@@ -2,6 +2,8 @@ import Task from "./Task";
 import { React, useState } from "react";
 import { v4 } from "uuid";
 
+// Okay so here's my idea on how to complete this whole tracking of roundsCompleted per Task--I think we can just calculate them based on current state. My idea is to mark the roundsCompleted when a Task is created, replacing the task.actual value with a task.initial value. Then wherever we want to reference the "actual" rounds completed, we can merely write roundsCompleted - task.initial. Then when it is marked as complete, we can create another state called task.final (which is roundsCompleted when the task is decommissioned), and task.final - task.initial should give the completed tasks their historical number of completedRounds per task. 
+
 function TaskContainer({ roundsCompleted }) {
   const [isHideModal, setIsHideModal] = useState(true);
   const [isHideEditModal, setIsHideEditModal] = useState(true);
@@ -13,14 +15,26 @@ function TaskContainer({ roundsCompleted }) {
     {
       name: "Rename Me",
       estimated: 1,
-      actual: 0,
+      initial: roundsCompleted,
+      final: 0,
       completed: false,
       id: placeholderId,
       modalHandler: modalHandler,
     },
   ]);
-  const [id, setId] = useState(placeholderId);
+  // task.initial = tasksList[task.]task.final + 1.
 
+  function getFinal(){
+    return roundsCompleted;
+  }
+  
+  function getInitialById(id){
+    const task = getTaskById(id);
+    const index = indexOf(task);
+    return tasksList[task - 1 ? task-1 : 0].final + 1.
+  }
+
+  const [id, setId] = useState(placeholderId);
   function modalHandler(type, id) {
     showModal(type);
     setId(id); // Once a user clicks on the open_list icon in the editModal, it will call this function, which will call setId(id), making that list item's id available in the state, so that it can be consumed by other components.
@@ -33,59 +47,45 @@ function TaskContainer({ roundsCompleted }) {
     return task;
   }
 
-  // EDIT MODAL
-  function EditModal() {
+  function actual(id){ 
     const task = getTaskById(id);
-    // const task = tasksList.find((task) =>
-    // {
-    //   console.log(task.id + "\n" + id);
-    //   console.log("Match? " + (task.id === id));
-    //   return (task.id === id )}
-    // );
-
-    return (
-      <div className="modal edit-modal">
-        <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            placeholder={task.name}
-            value={name}
-            onChange={handleNameChange}
-            className="input-field"
-          />
-          <br />
-          <span>
-            {task.actual}/{task.estimated} Pomodoros
-          </span>
-          <br />
-          <input
-            type="number"
-            placeholder={task.actual}
-            onChange={handleEstimatedChange}
-            className="input-field"
-            // value={actual}
-          />
-          <span>/</span>
-          <input
-            type="number"
-            value={estimated}
-            placeholder={task.estimated}
-            onChange={handleEstimatedChange}
-            className="input-field"
-          />
-          <button type="button" onClick={minusOne}>
-            <span className="material-icons icons">arrow_drop_down</span>
-          </button>
-          <button type="button" onClick={addOne}>
-            <span className="material-icons icons">arrow_drop_up</span>
-          </button>
-          <button onClick={() => deleteBtnClicked(id)}>Delete</button>
-          <button onClick={(e) => cancelBtnClicked(e)}>Cancel</button>
-          <button type="submit">Save</button>
-        </form>
-      </div>
-    );
+    console.log("Actual rounds completed: " + task.completed ? task.final-task.roundsCompleted : task.roundsCompleted-task.initial )
+    return task.completed? task.final-task.roundsCompleted : task.roundsCompleted-task.initial // This function checks whether the task is completed and returns the correct number...
   }
+
+// EDIT MODAL
+function EditModal() {
+  const task = getTaskById(id);
+  return (
+    <div className="modal edit-modal">
+    <form onSubmit={handleSubmit}>
+      <input type="text" placeholder={task.name}value={name} onChange={handleNameChange}className="input-field"
+      />
+      <br />
+      
+      <span>{actual(id)}/{task.estimated} Pomodoros</span>
+      <br />
+      <input
+        type="number" placeholder={actual(id).toString()} onChange={handleEstimatedChange} className="input-field"
+        // value={actual}
+      />
+      <span>/</span>
+      <input
+        type="number" value={estimated} placeholder={task.estimated} onChange={handleEstimatedChange}className="input-field"
+      />
+      <button type="button" onClick={minusOne}>
+        <span className="material-icons icons">arrow_drop_down</span>
+      </button>
+      <button type="button" onClick={addOne}>
+        <span className="material-icons icons">arrow_drop_up</span>
+      </button>
+      <button onClick={()=>deleteBtnClicked(id)}>Delete</button>
+      <button onClick={(e) => cancelBtnClicked(e)}>Cancel</button>
+      <button type="submit">Save</button>
+    </form>
+  </div>
+  )
+};
 
   function showModal(modalType) {
     if (modalType === "edit") {
