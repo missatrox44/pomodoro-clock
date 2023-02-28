c0e9c716f1daf966d1636ce4d43c3c68427be916
Developer <Developer@Johns-MacBook-Air-2.local>
2023-02-27 23:11:40 -0500

    Made changes to task data, removing Task.actual, replacing with task.initial and task.final, and also writing a function that calculated actual called actual(). It has already been implemented where needed. Also there are now two functions to calculate initial and final, which should be used in handler functions. They are getInitialById(id), and getFinal(). Note that the first requires the task's id, whereas the second one doesn't.

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-function Task({ name, estimated, actual, completed, id, modalHandler }) {
+function Task({ name, estimated, initial, final, completed, id, modalHandler, roundsCompleted }) {
   console.log("This is id from Task function: " + id);
   return (
     <div className="task-list-item" id={id}>
       <span>{name}</span>
       <span>
-        {actual}/{estimated}
+        {completed? final-roundsCompleted : roundsCompleted-initial}/{estimated}
       </span>
       <span>{completed ? "Completed" : "In progress"}</span>
       {/* <span 
--- src/components/TaskContainer.jsx
+++ src/components/TaskContainer.jsx
@@ -2,15 +2,29 @@ import Task from "./Task";
 import { React, useState } from "react";
 import { v4 } from "uuid";
 
+{/* Okay so here's my idea on how to complete this whole tracking of roundsCompleted per Task--I think we can just calculate them based on current state. My idea is to mark the roundsCompleted when a Task is created, replacing the task.actual value with a task.initial value. Then wherever we want to reference the "actual" rounds completed, we can merely write roundsCompleted - task.initial. Then when it is marked as complete, we can create another state called task.final (which is roundsCompleted when the task is decommissioned), and task.final - task.initial should give the completed tasks their historical number of completedRounds per task. */}
+
 function TaskContainer({ roundsCompleted }) {
   const [isHideModal, setIsHideModal] = useState(true);
   const [isHideEditModal, setIsHideEditModal] = useState(true);
   const [name, setName] = useState("");
   const [estimated, setEstimated] = useState(1);
   const placeholderId = v4()
-  // name, estimated, actual, completed, id, modalHandler
-  const [tasksList, setTasksList] = useState([{name:'Rename Me', estimated:1, actual:0, completed: false, id: placeholderId, modalHandler: modalHandler}]);
   const [id, setId] = useState(placeholderId);
+  // name, estimated, initial, final, completed, id, modalHandler
+  const [tasksList, setTasksList] = useState([{name:'Rename Me', estimated:1, initial:roundsCompleted, final:0, completed: false, id: placeholderId, modalHandler: modalHandler}]);
+  
+// task.initial = tasksList[task.]task.final + 1.
+
+function getFinal(){
+  return roundsCompleted;
+}
+
+function getInitialById(id){
+  const task = getTaskById(id);
+  const index = indexOf(task);
+  return tasksList[task - 1 ? task-1 : 0].final + 1.
+}
 
   function modalHandler(type, id){
     showModal(type);
@@ -23,26 +37,26 @@ function getTaskById(id){
     return task;
 }
 
+function actual(id){ 
+  const task = getTaskById(id);
+  console.log("Actual rounds completed: " + task.completed ? task.final-task.roundsCompleted : task.roundsCompleted-task.initial )
+  return task.completed? task.final-task.roundsCompleted : task.roundsCompleted-task.initial // This function checks whether the task is completed and returns the correct number...
+}
+
 // EDIT MODAL
   function EditModal() {
     const task = getTaskById(id);
-    // const task = tasksList.find((task) => 
-    // { 
-    //   console.log(task.id + "\n" + id);
-    //   console.log("Match? " + (task.id === id));
-    //   return (task.id === id )}
-    // ); 
-
     return (
       <div className="modal edit-modal">
       <form onSubmit={handleSubmit}>
         <input type="text" placeholder={task.name}value={name} onChange={handleNameChange}className="input-field"
         />
         <br />
-        <span>{task.actual}/{task.estimated} Pomodoros</span>
+        
+        <span>{actual(id)}/{task.estimated} Pomodoros</span>
         <br />
         <input
-          type="number" placeholder={task.actual} onChange={handleEstimatedChange} className="input-field"
+          type="number" placeholder={actual(id).toString()} onChange={handleEstimatedChange} className="input-field"
           // value={actual}
         />
         <span>/</span>
@@ -110,7 +124,7 @@ function deleteItem(id, array){
     e.preventDefault();
     // name, estimated, actual, completed, id,
     const newId = v4()
-    const task = { name, estimated, actual: 0, completed: false, id:newId, running: false };
+    const task = { name, estimated, initial: 0, final: 0, completed: false, id:newId, running: false };
     setTasksList([...tasksList, task]);
     setName("");
     setEstimated(1);
@@ -135,10 +149,12 @@ function deleteItem(id, array){
       <Task
         name={task.name}
         estimated={task.estimated}
-        actual={task.actual}
+        initial={task.initial}
+        final={task.final}
         completed={task.completed}
         id={task.id}
         modalHandler={modalHandler}
+        roundsCompleted={roundsCompleted}
         key={task.id}
       />
     );
