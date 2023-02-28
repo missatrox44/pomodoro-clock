0a48d9869950505ba97bd2afc9e8bd69548f4bd2
Sara Baqla <missatrox44@gmail.com>
2023-02-27 22:51:58 -0600

    added complete icon with simple boolean functionality

--- src/components/Pomodoro.jsx
+++ src/components/Pomodoro.jsx
@@ -7,11 +7,11 @@ export default function Pomodoro(){
     const [roundsCompleted, setRoundsCompleted] = useState(0);
 
     return (
-    <>
+    <div className='pomodoro'>
         <TimerContainer setRoundsCompleted={setRoundsCompleted} roundsCompleted={roundsCompleted}/>
         
         <TaskContainer roundsCompleted={roundsCompleted}/>
-    </>
+    </div>
     )
 }
 
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Task({ name, estimated, actual, completed, id, modalHandler }) {
   console.log("This is id from Task function: " + id);
+
+  const [isComplete, setIsComplete] = useState(false);
+  function handleClickIcon() {
+    setIsComplete(!isComplete);
+  }
+
   return (
     <div className="task-list-item" id={id}>
+      <div
+        onClick={() => handleClickIcon()}
+        className={`material-icons icons ${
+          isComplete ? "task-completed" : "in-progress"
+        }`}>
+        check_circle_outline
+      </div>
       <span>{name}</span>
       <span>
         {actual}/{estimated}
